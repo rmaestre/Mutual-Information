eb91f135962b663f43e179c6f4abb017e18eb97f
Roberto Maestre <rmaestre@gmail.com>
2012-03-31 10:08:57 +0200

    Class added

--- mi.py
+++ mi.py
@@ -8,40 +8,56 @@ from __future__ import division
 from numpy  import *
 import math
 
-# Define data array
-data = array( [ (1,1,1,0,0,0),
-                (0,0,0,1,1,1)] )
-
-# Check if all rows have the same length
-assert (len(data.shape) == 2)
-
-# Get basic info for loops
-print data.shape
-n_rows = data.shape[0]
-n_cols = data.shape[1]
-
-# Perform summation
-x_index = 0
-while x_index < n_rows :
-    y_index = x_index + 1
-    while y_index < n_rows:
-        # Final summation
+class MutualInformation:
+    
+    def __init__(self, data):
+        """
+        """
+        # Check if all rows have the same length
+        assert (len(data.shape) == 2)
+        # Save data
+        self.data = data
+        self.n_rows = data.shape[0]
+        self.n_cols = data.shape[1]
+        
+    def get_mi(self, x_index, y_index, debug = False):
+        """
+        Calculate and return Mutual information between two random variables
+        """
+        # Check if index are into the bounds
+        assert (x_index >= 0 and x_index <= self.n_rows)
+        assert (y_index >= 0 and y_index <= self.n_rows)
+        # Variable to return MI
         summation = 0.0
         # Get uniques values of random variables
         values_x = set(data[x_index])
         values_y = set(data[y_index])
-        print 'MI between'
-        print data[x_index]
-        print data[y_index]
+        if debug:
+            print 'MI between'
+            print data[x_index]
+            print data[y_index]
         # For each random
         for value_x in values_x:
             for value_y in values_y:
-                px = shape(where(data[x_index]==value_x))[1] / n_cols
-                py =shape(where(data[y_index]==value_y))[1] / n_cols
-                pxy = len(where(in1d(where(data[x_index]==value_x)[0], where(data[y_index]==value_y)[0])==True)[0]) / n_cols
+                px = shape(where(data[x_index]==value_x))[1] / self.n_cols
+                py =shape(where(data[y_index]==value_y))[1] / self.n_cols
+                pxy = len(where(in1d(where(data[x_index]==value_x)[0], where(data[y_index]==value_y)[0])==True)[0]) / self.n_cols
                 if pxy > 0.0:
                     summation += pxy * math.log((pxy / (px*py)), 2)
-                print '(%d,%d) px:%f py:%f pxy:%f' % (value_x, value_y, px, py, pxy)
-        print 'var_%d var_%d MI=%f\n\n' % (x_index, y_index, summation)
-        y_index += 1
-    x_index += 1
+                if debug:
+                    print '(%d,%d) px:%f py:%f pxy:%f' % (value_x, value_y, px, py, pxy)
+        if debug:
+            print 'var_%d var_%d MI=%f\n\n' % (x_index, y_index, summation)
+        return summation
+
+    
+    
+# Define data array
+data = array( [ (1,1,1,0,0,0),
+                (0,0,0,1,1,1),
+                (1,0,1,1,0,0)] )
+                
+mi = MutualInformation(data)
+print mi.get_mi(0,1)
+print ''
+print mi.get_mi(1,2, True)
